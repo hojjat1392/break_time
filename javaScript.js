384c132914874ef30c6b880e58a5fa345f53d721
hojjatAllah1371 <hojjat.rezapoor95@gmail.com>
2023-03-27 11:48:13 +0430

    break_time added

---
+++ javaScript.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+var date = new Date();
+var showDate = date.getDate();
+var showTime = date.get
+
+
+var showTime;
+var d, t;
+
+
+setInterval(() => {
+    document.getElementById('date').innerHTML = showDate,
+    document.getElementById('time').innerHTML = showTime
+}, 1000);
